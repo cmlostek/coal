ee15dfc9661cf98d567f4c696e8f0cedfc89dd5b
colemlostek <cmlostek@charlotte.edu>
2025-10-23 19:52:45 -0400

    Finished implementing the death and death log commands.

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,6 @@
 import discord
 import os
 from dotenv import load_dotenv
-import re
 from discord.ext.commands import *
 
 intents = discord.Intents.default()
@@ -16,7 +15,7 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    '''Responds with Pong! to test if the bot is responsive.'''
+    '''Responds with the bot latency! Use to test if the bot is responsive.'''
     await ctx.send(round(bot.latency, 2) * 1000)
 
 @bot.command()
@@ -26,10 +25,11 @@ async def greet(ctx):
 
 @bot.command()
 async def death(ctx, id, *, reason=None):
+    death_channel = bot.get_channel(1422284082955685888)  # Replace with your channel ID
     '''
     Logs the death, telling the user that they have died, optionally providing a reason.
-    :param reason: The reason for the death
-    :return:
+    :param id: The id of the user [REQUIRED]
+    :param reason: The reason for the death [OPTIONAL]
     '''
     # Open or create the death count file
     if not os.path.exists('death.txt'):
@@ -50,12 +50,12 @@ async def death(ctx, id, *, reason=None):
         else:
             user = await bot.fetch_user(id[2:-1])
             if reason:
-                f.write(f'{id} -- {num} - You have met a terrible fate, {user}. Reason: {reason}\n')
-                await ctx.send(f'{num} - You have met a terrible fate, {user}.\n Reason: {reason}')
+                f.write(f'{id} -- {num} - {user} has died.. Reason: {reason}\n')
+                await death_channel.send(f'{num} - You have met a terrible fate, {user}.\n Reason: {reason}')
                 await ctx.message.delete()
             else:
-                f.write(f'{id} -- {num} - You have met a terrible fate, {user}.\n')
-                await ctx.send(f'{num} - You have met a terrible fate, {user}.')
+                f.write(f'{id} -- {num} - {user} has died.\n')
+                await death_channel.send(f'{num} - You have met a terrible fate, {user}.')
                 await ctx.message.delete()
 @death.error
 async def death_error(ctx, error):
@@ -66,12 +66,17 @@ async def death_error(ctx, error):
 async def death_log(ctx, *, user_id=None):
     '''
     Retrieves and sends the death log for a specific user.
-    :param user_id: The ID of the user whose death log is to be retrieved
-    :return:
+    :param user_id: The ID of the user whose death log is to be retrieved. If None, retrieves the log for the command invoker. To send the entire log, use 1 as your user_id.
     '''
     # If no user_id is provided, use the command invoker's ID
     if user_id is None:
         user_id = ctx.author.id
+    elif user_id == '1':
+        try:
+            ctx.send(file=discord.File("./death_log.txt"))
+        except FileNotFoundError:
+            await ctx.send('Death log file not found.')
+        return
     try:
         logs = []
         user_logs = []
@@ -94,6 +99,24 @@ async def death_log(ctx, *, user_id=None):
     except FileNotFoundError:
         await ctx.send('Death log file not found.')
 
+@bot.command()
+async def quote(ctx, user_id, *, message):
+    quote_channel = bot.get_channel(1421296010231287858)
+    user = await bot.fetch_user(user_id[2:-1])
+    '''Sends the provided message as a quote.'''
+    quote_msg = discord.Embed(
+        title=f'{user} ',
+        description=message,
+        color=discord.Color.blue()
+    )
+    await ctx.send(embed=quote_msg)
+    await ctx.message.delete()
+
+@bot.command()
+async def echo(ctx, *, message):
+    '''Echoes the provided message back to the user.'''
+    await ctx.send(message)
+    await ctx.message.delete()
 
 if __name__ == '__main__':
     load_dotenv()
